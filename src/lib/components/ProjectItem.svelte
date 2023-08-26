2fdb7b871a99464513367d9eb25b02bfcca917f9
nasheomirro <shanenescarimoro@gmail.com>
2023-08-27 01:50:32 +0800

    redesigned projects section

--- src/lib/components/ProjectItem.svelte
+++ src/lib/components/ProjectItem.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Github from '$lib/icons/Github.svelte';
+
 	export let firstLink: string;
 	export let secondLink: string;
 	export let imgUrl: string;
@@ -6,12 +8,14 @@
 </script>
 
 <li class="w-full overflow-hidden border rounded-lg border-surface-800">
-	<div class="flex items-center h-8 gap-2 pl-5 md:h-10 bg-primary-500">
-    <div class="w-3 border rounded-full aspect-square border-surface-900 bg-surface-200"></div>
-    <div class="w-3 border rounded-full aspect-square border-surface-900 bg-surface-200"></div>
-    <div class="w-3 border rounded-full aspect-square border-surface-900 bg-surface-200"></div>
-  </div>
-	<div class="flex flex-col-reverse justify-between gap-4 p-4 md:flex-row md:p-6 lg:p-8 bg-surface-100">
+	<div class="flex items-center h-8 gap-2 pl-5 md:h-10 bg-surface-400">
+		<div class="w-3 border rounded-full aspect-square border-surface-900 bg-surface-200" />
+		<div class="w-3 border rounded-full aspect-square border-surface-900 bg-surface-200" />
+		<div class="w-3 border rounded-full aspect-square border-surface-900 bg-surface-200" />
+	</div>
+	<div
+		class="flex flex-col-reverse justify-between gap-4 p-4 md:flex-row md:p-6 lg:p-8 bg-surface-300"
+	>
 		<div class="mx-auto space-y-4 md:max-w-sm lg:max-w-xl xl:max-w-2xl md:mx-0">
 			<h3 class="h3">
 				<slot name="title" />
@@ -19,17 +23,14 @@
 			<p>
 				<slot name="description" />
 			</p>
-			<div class="flex justify-end gap-4 md:justify-start">
-				<a
-					href={firstLink}
-					target="_blank"
-					class="px-3 py-1 font-semibold border rounded-sm hover:brightness-110 border-surface-800 bg-primary-500"
-					>github</a
-				>
-				<a
-					href={secondLink}
-					target="_blank"
-					class="px-3 py-1 font-semibold border rounded-sm hover:brightness-110 border-surface-800 bg-primary-700"
+			<div class="flex justify-end gap-2 md:justify-start">
+				<a href={firstLink} target="_blank" class="button bg-surface-900 text-white pl-2">
+					<div class="w-6">
+            <Github />
+          </div>
+					<span> github </span>
+				</a>
+				<a href={secondLink} target="_blank" class="button bg-primary-500"
 					><slot name="second-button">
 						<span>live</span>
 					</slot>
